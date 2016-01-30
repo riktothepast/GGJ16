eace4bbd7d05676240822fb8602b2a26b2169166
unknown <moraleszez@gmail.com>
2016-01-30 13:29:47 -0600

    added nail placeholder

--- Assets/Scripts/BatMiniGame/BatLife/BatLife.cs
+++ Assets/Scripts/BatMiniGame/BatLife/BatLife.cs
@@ -8,12 +8,15 @@ public class BatLife : MonoBehaviour {
     public float points;
 
     public OnEvent onDamage;
+    
 
     public void ListenForHit(Vector3 position)
     {
         float distance = Vector3.Distance(transform.position, position);
-        points += Mathf.Clamp(10 - distance, 0, 10);
-        Debug.Log(distance);
-
+        points += Mathf.Clamp(30.12f - distance, 0, 0.12f)*100;
+        if(onDamage != null)
+        {
+            onDamage();
+        }
     }
 }
