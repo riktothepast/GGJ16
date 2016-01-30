0b93f2615e32842ba2ebfa853a6e29df978a7c98
riktothepast <rik@502studios.net>
2016-01-30 16:59:52 -0600

    incontrol and seasoning mini game.

--- Assets/Scripts/SeasoningMiniGame/HolderFiller.cs
+++ Assets/Scripts/SeasoningMiniGame/HolderFiller.cs
@@ -3,14 +3,33 @@ using System.Collections;
 
 public class HolderFiller : MonoBehaviour {
     public ProgressSlider pogressSlider;
+    public int mid = 10;
+    public float xCenter = 0f;
+    RightMovementController clawController;
+    public bool trackX, trackY;
+    void Start()
+    {
+        clawController = RightMovementController.CreateWithDefaultBindings();
+    }
 
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public void InitMovement()
+    { 
+        
+    }
+
 
     void OnParticleCollision(GameObject other)
     {
         pogressSlider.currentLife++;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (clawController.Move.IsPressed)
+        {
+            Vector3 touchPosition = transform.position + new Vector3(-clawController.Move.X, -clawController.Move.Y, 0);
+            transform.position = Vector3.Lerp(transform.position, new Vector3(trackX ? touchPosition.x : transform.position.x, trackY ? touchPosition.y : transform.position.y, transform.position.z), Time.deltaTime * 20.0f);
+        }
     }
 }
---
+++ Assets/Scripts/SeasoningMiniGame/MovementController.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+using InControl;
+
+public class MovementController : PlayerActionSet
+{
+    public PlayerAction Left;
+    public PlayerAction Right;
+    public PlayerAction Up;
+    public PlayerAction Down;
+    public PlayerTwoAxisAction Move;
+
+    public MovementController()
+    {
+        Left = CreatePlayerAction("Move Left");
+        Right = CreatePlayerAction("Move Right");
+        Up = CreatePlayerAction("Move Up");
+        Down = CreatePlayerAction("Move Down");
+        Move = CreateTwoAxisPlayerAction(Left, Right, Down, Up);
+    }
+
+    public static MovementController CreateWithDefaultBindings()
+    {
+        var playerController = new MovementController();
+        playerController.Up.AddDefaultBinding(Key.UpArrow);
+        playerController.Down.AddDefaultBinding(Key.DownArrow);
+        playerController.Left.AddDefaultBinding(Key.LeftArrow);
+        playerController.Right.AddDefaultBinding(Key.RightArrow);
+
+        playerController.Left.AddDefaultBinding(InputControlType.LeftStickLeft);
+        playerController.Right.AddDefaultBinding(InputControlType.LeftStickRight);
+        playerController.Up.AddDefaultBinding(InputControlType.LeftStickUp);
+        playerController.Down.AddDefaultBinding(InputControlType.LeftStickDown);
+
+        playerController.Left.AddDefaultBinding(InputControlType.DPadLeft);
+        playerController.Right.AddDefaultBinding(InputControlType.DPadRight);
+        playerController.Up.AddDefaultBinding(InputControlType.DPadUp);
+        playerController.Down.AddDefaultBinding(InputControlType.DPadDown);
+        playerController.ListenOptions.IncludeUnknownControllers = true;
+        playerController.ListenOptions.MaxAllowedBindings = 3;
+
+        playerController.ListenOptions.OnBindingFound = (action, binding) =>
+        {
+            if (binding == new KeyBindingSource(Key.Escape))
+            {
+                action.StopListeningForBinding();
+                return false;
+            }
+            return true;
+        };
+
+        playerController.ListenOptions.OnBindingAdded += (action, binding) =>
+        {
+            Debug.Log("Binding added... " + binding.DeviceName + ": " + binding.Name);
+        };
+
+        playerController.ListenOptions.OnBindingRejected += (action, binding, reason) =>
+        {
+            Debug.Log("Binding rejected... " + reason);
+        };
+
+        return playerController;
+    }
+}
