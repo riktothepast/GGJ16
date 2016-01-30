8d15380cfacc121a37d52df53d6d9bf80b0d9cfd
unknown <moraleszez@gmail.com>
2016-01-30 13:17:35 -0600

    added axe can hit assets

---
+++ Assets/Scripts/BatMiniGame/BatLife/BatLife.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public delegate void OnEvent();
+
+public class BatLife : MonoBehaviour {
+
+    public float points;
+
+    public OnEvent onDamage;
+
+    public void ListenForHit(Vector3 position)
+    {
+        float distance = Vector3.Distance(transform.position, position);
+        points += Mathf.Clamp(10 - distance, 0, 10);
+        Debug.Log(distance);
+
+    }
+}
