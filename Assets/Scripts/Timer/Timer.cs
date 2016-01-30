f53867b418c0b9d02c0b89481f616b257d745353
unknown <moraleszez@gmail.com>
2016-01-30 10:22:23 -0600

    adds timer Class, and timerClassManager

---
+++ Assets/Scripts/Timer/Timer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class Timer : MonoBehaviour {
+
+	public void StartTimer(float timeLimit, Action callback)
+    {
+        StartCoroutine(TimerCoroutine(timeLimit, callback));
+    }
+
+    IEnumerator TimerCoroutine(float timeLimit, Action callback)
+    {
+        yield return new WaitForSeconds(timeLimit);
+        if(callback != null)
+        {
+            callback();
+        }
+    }
+}
