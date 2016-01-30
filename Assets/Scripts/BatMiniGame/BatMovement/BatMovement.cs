b23e3fd73c3703502ba59d8f29a982057f392386
unknown <moraleszez@gmail.com>
2016-01-30 11:42:39 -0600

    bat now has logic to bounc different ways!

--- Assets/Scripts/BatMiniGame/BatMovement/BatMovement.cs
+++ Assets/Scripts/BatMiniGame/BatMovement/BatMovement.cs
@@ -4,7 +4,8 @@ using System.Collections;
 public enum CollState
 {
     Right,
-    Left
+    Left,
+    None
 }
 
 public class BatMovement : MonoBehaviour {
@@ -14,16 +15,20 @@ public class BatMovement : MonoBehaviour {
     public float radius;
     protected bool grounded;
     protected bool jumping;
-
+    protected float dir = 0;
+    protected CollState collState;
     void Start()
     {
         TimeClassManager.StartTimer(3, JumpLogic);
         rigidBody2d = GetComponent<Rigidbody2D>();
+        dir = 1;
+        collState = CollState.Right;
     }
 
     void Update()
     {
         CheckGround();
+        UpdateCollState();
         JumpLogic();
     }
 
@@ -31,7 +36,14 @@ public class BatMovement : MonoBehaviour {
     {
         if (grounded)
         {
-            rigidBody2d.AddForce(new Vector2(0.5f, 1) * 7, ForceMode2D.Impulse);
+            float random = Random.Range(1, 7) + Random.Range(1, 7) + Random.Range(1, 7);
+            if(random < 5)
+            {
+                rigidBody2d.AddForce(new Vector2(0, 1) * 10, ForceMode2D.Impulse);
+            }
+            else {
+                rigidBody2d.AddForce(new Vector2(0.5f * dir, 1) * 7, ForceMode2D.Impulse);
+            }
         }
     }
 
@@ -48,8 +60,31 @@ public class BatMovement : MonoBehaviour {
         }
     }
 
+    void UpdateCollState()
+    {
+        RaycastHit2D hitRight = Physics2D.Raycast(transform.position, Vector3.right, radius, layerMask);
+        RaycastHit2D hitLeft = Physics2D.Raycast(transform.position, Vector3.left, radius, layerMask);
+        if(hitRight.collider != null)
+        {
+            dir = -1;
+            collState = CollState.Right;
+        }
+        else if(hitLeft.collider != null)
+        {
+            collState = CollState.Left;
+            dir = 1;
+        }
+        else
+        {
+            collState = CollState.None;
+        }
+    }
+
     void OnDrawGizmos()
     {
         Gizmos.DrawRay(transform.position, Vector3.down * radius);
+        Gizmos.DrawRay(transform.position, Vector3.right * radius);
+        Gizmos.DrawRay(transform.position, Vector3.left * radius);
+
     }
 }
