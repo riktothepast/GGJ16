5504cb4476a19239e2e1546969754f6289418b80
riktothepast <rik@502studios.net>
2016-01-30 14:21:01 -0600

    adds progress slider.

---
+++ Assets/Scripts/Utils/ProgressSlider.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ProgressSlider : MonoBehaviour {
+
+	Slider slider;
+    public float totalLife, currentLife;
+
+	// Use this for initialization
+	void Awake () {
+		slider = GetComponent<Slider>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+       float progress = (float)currentLife / (float) totalLife;
+	   slider.value = progress;
+	}
+}
