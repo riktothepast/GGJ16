23c08225120131a2ac2e9bcb217f60e19c07ad5d
riktothepast <rik@502studios.net>
2016-01-31 10:42:59 -0600

    adds shake manager class

---
+++ Assets/Scripts/Utils/ShakeManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using Prime31.ZestKit;
+
+public class ShakeManager : MonoBehaviour {
+    CameraShakeTween shake;
+
+	void Start () {
+        shake = new CameraShakeTween(Camera.main);
+	}
+
+    void DoCameraShake(float intensity = 10f)
+    { 
+         shake.shake(intensity);
+         Debug.Log("shake");
+    }
+}
